3c77bb9d5950b5aca1d111f809c8d2599b1f78f1
Brendan Slabe <slabe@google.com>
2025-05-08 16:19:10 +0000

    printing config

--- inference_perf/config.py
+++ inference_perf/config.py
@@ -125,5 +125,7 @@ def read_config() -> Config:
 
         default_cfg = Config().model_dump()
         merged_cfg = deep_merge(default_cfg, cfg)
+
+        print(f"Benchmarking with the following config:\n{yaml.dump(merged_cfg.model_dump(), sort_keys=False, default_flow_style=False)}")
         return Config(**merged_cfg)
     return Config()
