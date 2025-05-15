eacf6823d5a2446f1fbfceb815c40a9da0644eb8
Brendan Slabe <slabe@google.com>
2025-05-15 17:04:31 +0000

    first commit

--- inference_perf/config.py
+++ inference_perf/config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 from typing import Any, Optional, List
 from argparse import ArgumentParser
 from enum import Enum
@@ -90,7 +90,7 @@ class ReportConfig(BaseModel):
 
 class PrometheusClientConfig(BaseModel):
     scrape_interval: int = 15
-    url: str = "http://localhost:9090"
+    url: HttpUrl = HttpUrl(url="http://localhost:9090")
 
 
 class MetricsClientConfig(BaseModel):
