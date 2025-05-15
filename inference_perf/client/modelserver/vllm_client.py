abdec3299248a6c7d6ad774343767407492b161c
Brendan Slabe <slabe@google.com>
2025-05-15 21:48:09 +0000

    missing licence statements

--- inference_perf/client/modelserver/vllm_client.py
+++ inference_perf/client/modelserver/vllm_client.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from inference_perf.collectors.request_lifecycle import PromptLifecycleMetricsCollector
 from inference_perf.config import APIType
 from inference_perf.prompts.base import FailedResponseData, LlmPrompt, PromptLifecycleMetric, ResponseData
--- inference_perf/client/storage/base.py
+++ inference_perf/client/storage/base.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from abc import ABC, abstractmethod
 from typing import List
 from inference_perf.config import StorageConfigBase
-from inference_perf.reportgen import ReportFile
+from inference_perf.report.base import ReportFile
 
 
 class StorageClient(ABC):
