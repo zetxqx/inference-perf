3b0632813bac64ab4fc0a53b1ede57aaa050aaaa
Brendan Slabe <slabe@google.com>
2025-05-16 15:22:07 +0000

    nits

--- inference_perf/collectors/request_lifecycle.py
+++ inference_perf/collectors/request_lifecycle.py
@@ -44,7 +44,7 @@ class PromptLifecycleMetricsCollector(MetricsCollector):
                 )
 
         if report_config.per_stage:
-            print("Generating a per stage report of request lifecycle summary metrics")
+            print("Generating a per stage report of request lifecycle metrics")
             stage_buckets: dict[int, List[PromptLifecycleMetric]] = defaultdict(list)
             for metric in self.metrics:
                 if metric.stage_id is not None:
@@ -55,7 +55,7 @@ class PromptLifecycleMetricsCollector(MetricsCollector):
                 )
 
         if report_config.per_request:
-            print("Generating a per request report of request lifecycle summary metrics")
+            print("Generating a per request report of request lifecycle metrics")
             reports.append(ReportFile(name="per_request", contents=[metric.model_dump() for metric in self.metrics]))
 
         return reports
