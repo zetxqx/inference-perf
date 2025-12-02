dc512a1cbf19bf3e6eb673d133452b65514be5fa
Radhika Lakhtakia <137429298+rlakhtakia@users.noreply.github.com>
2025-12-02 14:48:55 -0800

    Update attribute error when parsing billsum conversations (#297)

--- inference_perf/datagen/hf_billsum_datagen.py
+++ inference_perf/datagen/hf_billsum_datagen.py
@@ -12,35 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-from inference_perf.apis import InferenceAPIData, CompletionAPIData, ChatCompletionAPIData, ChatMessage
-from inference_perf.utils.custom_tokenizer import CustomTokenizer
-from .base import DataGenerator
-from inference_perf.config import APIConfig, APIType, DataConfig
-from typing import Generator, List, Optional
-from datasets import load_dataset
 import os
+from typing import Generator, List, Optional
+
+from datasets import load_dataset
+from inference_perf.apis import ChatCompletionAPIData, ChatMessage, CompletionAPIData, InferenceAPIData
+from inference_perf.config import APIConfig, APIType, DataConfig
+from inference_perf.utils.custom_tokenizer import CustomTokenizer
+
+from .base import DataGenerator
 
 logger = logging.getLogger(__name__)
 
 
 class BillsumConversationsDataGenerator(DataGenerator):
-    def __init__(self, api_config: APIConfig, config: DataConfig, tokenizer: Optional[CustomTokenizer]) -> None:
+    def __init__(
+        self,
+        api_config: APIConfig,
+        config: DataConfig,
+        tokenizer: Optional[CustomTokenizer],
+    ) -> None:
         super().__init__(api_config, config, tokenizer)
-
-        if config.path is not None:
-            # check if the path is valid
-            if not os.path.exists(config.path):
-                raise ValueError(f"Invalid dataset path: {config.path}. Path does not exist.")
-            # depending on whether the dataset is a single file or a directory, we need to load it differently
-            if os.path.isfile(config.path) and config.path.endswith(".json"):
-                self.billsum_dataset = iter(load_dataset("json", data_files=config.path, streaming=True, split="train"))
-            elif os.path.isdir(config.path):
-                json_files = [f for f in os.listdir(config.path) if f.endswith(".json")]
-                self.billsum_dataset = iter(load_dataset("json", data_files=json_files, streaming=True, split="train"))
-            else:
-                raise ValueError(f"Invalid dataset path: {config.path}")
-        else:
-            raise ValueError("Invalid dataset path: No dataset path provided")
+        self.config = config
+        self._initialize_dataset()
 
         self.min_num_turns = 2
         self.data_key = "conversations"
@@ -50,13 +44,33 @@ class BillsumConversationsDataGenerator(DataGenerator):
         # Advance the iterator to the first data point
         next(self.billsum_dataset)
 
+    def _initialize_dataset(self) -> None:
+        if self.config.path is None:
+            raise ValueError("Invalid dataset path: No dataset path provided")
+        # check if the path is valid
+        if not os.path.exists(self.config.path):
+            raise ValueError(f"Invalid dataset path: {self.config.path}. Path does not exist.")
+        # depending on whether the dataset is a single file or a directory, we need to load it differently
+        if os.path.isfile(self.config.path) and self.config.path.endswith(".json"):
+            self.billsum_dataset = iter(load_dataset("json", data_files=self.config.path, streaming=True, split="train"))
+        elif os.path.isdir(self.config.path):
+            json_files = [f for f in os.listdir(self.config.path) if f.endswith(".json")]
+            self.billsum_dataset = iter(load_dataset("json", data_files=json_files, streaming=True, split="train"))
+        else:
+            raise ValueError(f"Invalid dataset path: {self.config.path}")
+
     def get_supported_apis(self) -> List[APIType]:
         return [APIType.Chat, APIType.Completion]
 
     def get_data(self) -> Generator[InferenceAPIData, None, None]:
         if self.billsum_dataset is not None:
             while True:
-                data = next(self.billsum_dataset)
+                try:
+                    data = next(self.billsum_dataset)
+                except StopIteration:
+                    self._initialize_dataset()
+                    data = next(self.billsum_dataset)
+
                 if (
                     data is None
                     or data[self.data_key] is None
@@ -93,7 +107,10 @@ class BillsumConversationsDataGenerator(DataGenerator):
                 elif self.api_config.type == APIType.Chat:
                     yield ChatCompletionAPIData(
                         messages=[
-                            ChatMessage(role=conversation[self.role_key], content=conversation[self.content_key])
+                            ChatMessage(
+                                role=conversation[self.role_key],
+                                content=conversation[self.content_key],
+                            )
                             for conversation in data[self.data_key]
                         ]
                     )
